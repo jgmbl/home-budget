2cdbe14b05b4a65a240e042df5b4a11dd8de5a69
jgmbl <joannagembel@gmail.com>
2023-09-12 11:21:07 +0200

    Add database connection

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+import db
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 # Configure application
 app = Flask(__name__)
@@ -10,4 +14,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
+# initialize database
+app.config['DATABASE'] = 'budget.db'
+database = db.init_app(app)

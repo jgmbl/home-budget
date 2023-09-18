204742a79a65a91e3a99c9672a0bfc42162c2102
jgmbl <joannagembel@gmail.com>
2023-09-18 17:04:04 +0200

    app.py and templates/login.html and templates/register.html: add login and register

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
-from flask_login import LoginManager, UserMixin
-
+from flask_login import LoginManager, UserMixin, login_user, LoginManager, logout_user, current_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 #configure database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-#configure app
+
+#configure security session cookie
 app.config['SECRET_KEY'] = 'e338ad3e3496373c1be998218674fa2876bd05fb95879f17d4e10b4b79124416'
 
-#configure login manager
+
+#configure log in
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
 
 
 @login_manager.user_loader
-def load_user(user):
-    return User.query.get(int(user))
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 #table user
@@ -33,41 +40,68 @@ class User(db.Model, UserMixin):
         return '<User %r>' % self.username
     
 
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={"placeholder": "Username"})
+    password = StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={"placeholder": "Username"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("Username is taken")
+        
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={"placeholder": "Username"})
+    password = StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
 @app.route("/", methods=["GET", "POST"])
+@login_required
 def index():
     return render_template("homepage.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-    session.clear()
+    form = LoginForm()
 
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        stmt = User()
-        session['user_id'] = rows[0][0]
-
-        return redirect("/")
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user:
+                if bcrypt.check_password_hash(user.password, form.password.data):
+                    login_user(user)
+                    return redirect("/")
     
     else:
-        return render_template("login.html")
+        return render_template("login.html", form=form)
 
 
-"""@app.route("/register", methods=["GET", "POST"])
+@app.route("/logout", methods = ["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    form = RegisterForm()
+
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        confirm_password = request.form.get("confirm_password")
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
 
-        return redirect("/")
-    
+            return redirect("/login")
+
     else:
-        return render_template("register.html")"""
-
+        return render_template("register.html", form=form)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)

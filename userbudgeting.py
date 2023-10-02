6af2daeefdcdebb65abf33b456eec184795916f1
jgmbl <joannagembel@gmail.com>
2023-10-02 12:24:58 +0200

    userbudgeting.py: add show_budgeting

--- userbudgeting.py
+++ userbudgeting.py
@@ -9,19 +9,6 @@ class UserBudgeting:
     def __init__(self, value_income):
         self.value_income = value_income
 
-    """Check if sum of percents is equal 100"""
-    @staticmethod
-    def check_sum_of_percent(*argv):
-        sum_percent = 0
-
-        for args in argv:
-            sum_percent += args
-        
-        if sum_percent != FULL_PERCENTAGE:
-            return False
-        
-        return True
-    
 
     """Convert percent value to numerical value"""
     def __value_percent_to_value_budgeting(self, value_percent):
@@ -45,6 +32,39 @@ class UserBudgeting:
         budgeting = cur.execute("INSERT INTO  budgeting(user_id, income, category, value, value_percent, date) VALUES (?, ?, ?, ?, ?, ?)", (user_id, self.value_income, category, value, value_percent, date))
         con.commit()
         con.close()
+
+
+    """Check if sum of percents is equal 100"""
+    @staticmethod
+    def check_sum_of_percent(*argv):
+        sum_percent = 0
+
+        for args in argv:
+            sum_percent += args
+        
+        if sum_percent != FULL_PERCENTAGE:
+            return False
+        
+        return True
+    
+    
+    """Show category, value and value_percent from table budgeting to table on website"""
+    def show_budgeting(self):
+        #user_id = session["_user_id"]
+        user_id = 1
+
+        con = sqlite3.connect("instance/budget.db")
+        cur = con.cursor()
+        
+
+        budgeting = cur.execute("SELECT category, value, value_percent FROM budgeting WHERE user_id = ?", (user_id,))
+        budgeting_table = budgeting.fetchall()
+        con.close()
+
+        return budgeting_table
+
+
+
 
 
 

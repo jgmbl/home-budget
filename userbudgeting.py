adc22088afcca79166d5201412e1f310b9e530ff
jgmbl <joannagembel@gmail.com>
2023-10-02 09:43:34 +0200

    userbudgeting.py: add add_budgeting_to_table

--- userbudgeting.py
+++ userbudgeting.py
@@ -1,3 +1,7 @@
+import sqlite3
+import datetime
+
+
 FULL_PERCENTAGE = 100
 
 class UserBudgeting:
@@ -17,6 +21,7 @@ class UserBudgeting:
         
         return True
     
+
     """Convert percent value to numerical value"""
     def __value_percent_to_value_budgeting(self, value_percent):
         value_numerical = value_percent / FULL_PERCENTAGE
@@ -25,8 +30,19 @@ class UserBudgeting:
         return value_budgeting
     
 
-    
+    """Add budgeting from form to table budgeting"""
+    def add_budgeting_to_table(self, user_id, category, value_percent):
 
+        value = self.__value_percent_to_value_budgeting(value_percent)
+        date = datetime.datetime.now()
+        date = date.strftime('%Y-%m-%d %H:%M')
+
+        con = sqlite3.connect("instance/budget.db")
+        cur = con.cursor()
+
+        budgeting = cur.execute("INSERT INTO  budgeting(user_id, income, category, value, value_percent, date) VALUES (?, ?, ?, ?, ?, ?)", (user_id, self.value_income, category, value, value_percent, date))
+        con.commit()
+        con.close()
 
 
 

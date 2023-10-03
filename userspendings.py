311aba55455f891216b51f783a636d2dbd8602a6
jgmbl <joannagembel@gmail.com>
2023-10-03 09:41:13 +0200

    userspendings.py: add getting spendings from last month

--- userspendings.py
+++ userspendings.py
@@ -6,13 +6,29 @@ class UserSpendings:
     """Add spendings from form to table spendings"""
     def add_spendings_to_table(self, category, value, note):
 
-        date = datetime.datetime.now()
-        date = date.strftime('%Y-%m-%d %H:%M')
+        current_date_and_time = datetime.datetime.now()
+        current_date = current_date_and_time.strftime('%Y-%m-%d %H:%M')
         user_id = session["_user_id"]
 
         con = sqlite3.connect("instance/budget.db")
         cur = con.cursor()
 
-        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, category, value, note, date))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, category, value, note, current_date))
         con.commit()
         con.close()
+
+
+    def get_spendings_from_last_month(self):
+        today = datetime.datetime.today()
+        current_month = today.strftime("%m")
+
+        user_id = session["_user_id"]
+
+        con = sqlite3.connect("instance/budget.db")
+        cur = con.cursor()
+
+        select_spendings = cur.execute("SELECT category, value, date FROM spendings WHERE user_id = ? AND strftime('%m', date) = ?", (user_id, current_month))
+        selected_spendings = select_spendings.fetchall()
+        con.close()
+
+        return selected_spendings

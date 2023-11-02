fd52628f83d4b2b05fce1907478c0cc7ac2ccf9f
jgmbl <joannagembel@gmail.com>
2023-11-02 12:48:56 +0100

    test_userspendings.py: add test of getting spendings from current week

--- test_userspendings.py
+++ test_userspendings.py
@@ -9,10 +9,16 @@ class TestUserSpendings(unittest.TestCase, UserSpendings):
         user_id = 1
         database = "test_budget.db"
 
-        self.add_spendings_to_table("investments", 10.0, "Test", user_id, database)
-
         con = sqlite3.connect(database)
         cur = con.cursor()
+
+        #delete data from table spendings
+        cur.execute("DELETE FROM spendings;")
+        con.commit()
+
+        self.add_spendings_to_table("investments", 10.0, "Test", user_id, database)
+
+        #select data from table spendings
         data = cur.execute("SELECT user_id, category, value, note FROM spendings ORDER BY date DESC LIMIT 1;")
 
         result_from_database = data.fetchall()
@@ -27,14 +33,18 @@ class TestUserSpendings(unittest.TestCase, UserSpendings):
         user_id = 1
         database = "test_budget.db"
 
-        #get current month number
+        #current date
         today = datetime.datetime.today()
         current_date = today.strftime('%Y-%m-%d %H:%M')
 
-        #get last month date
+        #get previous month date
         first_day = today.replace(day=1)
-        last_month = first_day - datetime.timedelta(days=1)
-        last_month = last_month.strftime('%Y-%m-%d %H:%M')
+        previous_month = first_day - datetime.timedelta(days=1)
+        previous_month = previous_month.strftime('%Y-%m-%d %H:%M')
+
+        #get next month date
+        next_month = first_day + datetime.timedelta(days=30)
+        next_month = next_month.strftime('%Y-%m-%d %H:%M')
 
         con = sqlite3.connect(database)
         cur = con.cursor()
@@ -47,8 +57,11 @@ class TestUserSpendings(unittest.TestCase, UserSpendings):
         cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 10.0, "test0", current_date))
         cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "large_spendings", 20.0, "test1", current_date))
         cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "investment", 20.0, "test2", current_date))
-        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "education", 50.0, "test3", last_month))
-        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 30.0, "test4", last_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "education", 50.0, "test3", previous_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 30.0, "test4", previous_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "other", 40.0, "test5", next_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 30.0, "test6", next_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "education", 20.0, "test7", next_month))
         con.commit()
 
         con.close()
@@ -58,6 +71,49 @@ class TestUserSpendings(unittest.TestCase, UserSpendings):
 
         expected_result = [('daily_spendings', 0.1, 'test0', current_date), ('large_spendings', 0.2, 'test1', current_date), ('investment', 0.2, 'test2', current_date)]
 
+        self.assertEqual(result_from_database, expected_result)
+
+
+    def test_get_spendings_from_current_week(self):
+        user_id = 1
+        database = "test_budget.db"
+
+        #current date
+        today = datetime.datetime.today()
+        current_date = today.strftime('%Y-%m-%d %H:%M')
+
+        #get previous month date
+        first_day = today.replace(day=1)
+        previous_month = first_day - datetime.timedelta(days=7)
+        previous_month = previous_month.strftime('%Y-%m-%d %H:%M')
+
+        #get next month date
+        next_month = first_day + datetime.timedelta(days=30)
+        next_month = next_month.strftime('%Y-%m-%d %H:%M')
+
+        con = sqlite3.connect(database)
+        cur = con.cursor()
+
+        #delete data from table spendings
+        cur.execute("DELETE FROM spendings;")
+        con.commit()
+
+        #add data to table spendings
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 10.0, "test0", current_date))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "large_spendings", 20.0, "test1", current_date))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "investment", 20.0, "test2", current_date))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "education", 50.0, "test3", previous_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "daily_spendings", 30.0, "test4", previous_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "other", 40.0, "test5", next_month))
+        cur.execute("INSERT INTO spendings(user_id, category, value, note, date) VALUES (?, ?, ?, ?, ?)", (user_id, "large_spendings", 15.0, "test6", next_month))
+        
+        con.commit()
+        con.close()
+
+        #get spendings by tested method
+        result_from_database = self.get_spendings_from_current_week(user_id, database)
+
+        expected_result = [('daily_spendings', 0.1, 'test0', current_date), ('large_spendings', 0.2, 'test1', current_date), ('investment', 0.2, 'test2', current_date)]
         self.assertEqual(result_from_database, expected_result)
 
 
